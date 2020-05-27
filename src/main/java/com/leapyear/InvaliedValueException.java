dc8fbd64c2d9eaf6dfff45c5dabd3e16e539646c
Dharmendra Awasthi <dharmendrakawasthi@gmail.com>
2020-05-27 21:19:15 +0530

    Final commit

--- src/main/java/com/leapyear/InvaliedValueException.java
+++ src/main/java/com/leapyear/InvaliedValueException.java
@@ -5,10 +5,4 @@ public class InvaliedValueException extends RuntimeException {
 	public InvaliedValueException(String s) {
 		super(s);
 	}
-    public InvaliedValueException(String s, Throwable throwable) {
-        super(s, throwable);
-    }
-    public InvaliedValueException(Throwable throwable) {
-        super(throwable);
-    }
 }
